0125bd80770704ffede2e870e113d3f06583c0ac
dieuhien1036 <dieuhien1036@gmail.com>
2024-06-09 12:35:14 +0700

    Fix login invalid credential will return 401 instead of 403

--- starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerficationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerficationFilter.java
@@ -8,8 +8,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,6 @@ import com.auth0.jwt.JWT;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
-@Component
 public class JWTAuthenticationVerficationFilter extends BasicAuthenticationFilter {
 
     public JWTAuthenticationVerficationFilter(AuthenticationManager authManager) {
